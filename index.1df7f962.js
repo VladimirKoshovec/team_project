3b570953a9251e49297a57c90ffbbf4261a24dc4
VladimirKoshovec <107856906+VladimirKoshovec@users.noreply.github.com>
2022-09-04 08:55:46 +0000

    Deploying to gh-pages from @ VladimirKoshovec/team_project@d202e38afd3c25528ba28b0b938ea44d9c092678 🚀

--- index.1df7f962.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[menu-mobile-open]"),closeModalBtn:document.querySelector("[menu-mobile-close]"),modal:document.querySelector("[menu-mobile]")};function o(){e.modal.classList.toggle("mobile--open")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.1df7f962.js.map
