ade83e964a37b18b37e8f42f2cc77d74a4a25c40
VladimirKoshovec <koshovec.vladimir@gmail.com>
2022-09-05 23:12:00 +0300

    add-menu-skript

--- src/js/test.js
+++ src/js/test.js
@@ -3,11 +3,21 @@
     const refs = {
       openModalBtnMenu: document.querySelector("[menu-mobile-open]"),
       closeModalBtnMenu: document.querySelector("[menu-mobile-close]"),
+      closeModalBtnMenu1: document.querySelector("[menu-mobile-close-link1]"),
+      closeModalBtnMenu2: document.querySelector("[menu-mobile-close-link2]"),
+      closeModalBtnMenu3: document.querySelector("[menu-mobile-close-link3]"),
+      closeModalBtnMenu4: document.querySelector("[menu-mobile-close-link4]"),
+      closeModalBtnMenu5: document.querySelector("[menu-mobile-close-link5]"),
       modalMenu: document.querySelector("[menu-mobile]"),
     };
   
     refs.openModalBtnMenu.addEventListener("click", toggleModal);
     refs.closeModalBtnMenu.addEventListener("click", toggleModal);
+    refs.closeModalBtnMenu1.addEventListener("click", toggleModal);
+    refs.closeModalBtnMenu2.addEventListener("click", toggleModal);
+    refs.closeModalBtnMenu3.addEventListener("click", toggleModal);
+    refs.closeModalBtnMenu4.addEventListener("click", toggleModal);
+    refs.closeModalBtnMenu5.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modalMenu.classList.toggle("mobile--open");
